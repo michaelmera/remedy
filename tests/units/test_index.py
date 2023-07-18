8e657624ba76a3ca34374d84dc2352a6f9d8e154
Michael Mera <dev@michaelmera.com>
2023-07-18 14:19:42 +0200

    test: add unit tests for special files in index

--- tests/units/test_index.py
+++ tests/units/test_index.py
@@ -1,11 +1,15 @@
 from assertpy import assert_that
 from remedy.remarkable.filesource import FileSource
-from remedy.remarkable.metadata import RemarkableIndex
+from remedy.remarkable.metadata import RemarkableIndex, ROOT_ID, TRASH_ID
 
 
 class MemorySource(FileSource):
+    def __init__(self) -> None:
+        super().__init__()
+        self.items = {}
+
     def listItems(self):
-        return {}
+        yield from self.items
 
 
 def test_index_has_root_folder() -> None:
@@ -13,3 +17,30 @@ def test_index_has_root_folder() -> None:
     index = RemarkableIndex(source)
 
     assert_that(index.root()).is_not_none()
+    assert_that(index.root().uid).is_equal_to(ROOT_ID)
+    assert_that(index.root().typeName()).is_equal_to("folder")
+
+
+def test_root_folder_has_no_parent() -> None:
+    source = MemorySource()
+    index = RemarkableIndex(source)
+
+    assert_that(index.root()).is_not_none()
+    assert_that(index.root().parentEntry()).is_none()
+
+
+def test_index_has_trash_folder() -> None:
+    source = MemorySource()
+    index = RemarkableIndex(source)
+
+    assert_that(index.trash).is_not_none()
+    assert_that(index.trash.uid).is_equal_to(TRASH_ID)
+    assert_that(index.trash.typeName()).is_equal_to("trash")
+
+
+def test_trash_folder_has_no_parent() -> None:
+    source = MemorySource()
+    index = RemarkableIndex(source)
+
+    assert_that(index.trash).is_not_none()
+    assert_that(index.trash.parentEntry()).is_none()
